20362589aff23f5516b56564830446b8acfcbc14
Mohamed Hussein <mohamed.hussein.tawfick@gmail.com>
2025-03-24 04:55:05 -0500

    Lab 06 update

---
+++ 06-Lab/F/Lab06F/main/java/counter/Counter.java
@@ -0,0 +1,17 @@
+package Lab05F.main.java.counter;
+
+public class Counter extends Subject{
+	
+	private int count=0;
+	
+	public void increment(){
+		count++;
+    	donotify(count);
+	}
+	
+	public void decrement(){
+		count--;
+    	donotify(count);
+	}
+
+}
---
+++ 06-Lab/F/Lab06F/main/java/counter/Observer.java
@@ -0,0 +1,5 @@
+package Lab05F.main.java.counter;
+
+public interface Observer {
+	public void update(int countervalue);
+}
